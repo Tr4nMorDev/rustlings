e81a4af76a262fa8dca365c1c58685174fdd465d
Tr4nMorDev <tranphongzhouzhou@gmail.com>
2025-02-02 18:28:38 +0700

    done_vecs

--- exercises/05_vecs/vecs2.rs
+++ exercises/05_vecs/vecs2.rs
@@ -4,16 +4,23 @@ fn vec_loop(input: &[i32]) -> Vec<i32> {
     for element in input {
         // TODO: Multiply each element in the `input` slice by 2 and push it to
         // the `output` vector.
+        output.push(*element);
     }
 
     output
+        .iter()
+        .map(|&e| e * 2)
+        .collect()
 }
 
 fn vec_map_example(input: &[i32]) -> Vec<i32> {
     // An example of collecting a vector after mapping.
     // We map each element of the `input` slice to its value plus 1.
     // If the input is `[1, 2, 3]`, the output is `[2, 3, 4]`.
-    input.iter().map(|element| element + 1).collect()
+    input
+        .iter()
+        .map(|&element| element + 1)
+        .collect()
 }
 
 fn vec_map(input: &[i32]) -> Vec<i32> {
@@ -24,7 +31,7 @@ fn vec_map(input: &[i32]) -> Vec<i32> {
     input
         .iter()
         .map(|element| {
-            // ???
+           *element * 2 
         })
         .collect()
 }
