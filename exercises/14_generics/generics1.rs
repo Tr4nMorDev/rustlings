6ba36cf368746fe9254e37733cf9b6fae3749c08
Tr4nMorDev <tranphongzhouzhou@gmail.com>
2025-02-13 15:45:04 +0700

    done_generics

--- exercises/14_generics/generics1.rs
+++ exercises/14_generics/generics1.rs
@@ -9,7 +9,7 @@ fn main() {
     let mut numbers:Vec<i32> = Vec::new();
 
     // Don't change the lines below.
-    let n1: u8 = 42;
+    let n1= 42;
     numbers.push(n1.into());
     let n2: i8 = -1;
     numbers.push(n2.into());
